41795087ada3a87f128339126084059cb666c94d
prakash-ces <prakash.perumalmariappan@cesltd.com>
2018-10-29 17:03:29 +0530

    stable changes - modified global text

--- scripts/globals.js
+++ scripts/globals.js
@@ -9,6 +9,8 @@ var gb_homeContent = "<div class='padding-top-5-p'>\
                         </header>\
                       </div>\
                       <form class='text-center padding-top-5-p' id='magicForm'>\
-                        <input class='padding-15 margin-5' id='inpMagicField' type='text' value='' placeholder='Enter the Magic Word' autocomplete='off' required/>\
+                        <input class='padding-15 margin-5' id='inpMagicField' type='text' value='' placeholder='Enter the Magic Sentence' autocomplete='off' required/>\
                         <input id='inpGetBtn' class='internal-button' type='submit' value='Show' onclick='db_get()' /> \
+                        <br/><br/><br/>(or else) \
+                        <br/><br/><br/>Sign-in to view all that is shared with you \
                       </form>";
